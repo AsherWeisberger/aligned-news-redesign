7fb397398c33d2769c5c02f81341b2bb4a92d374
AsherWeisberger <asherweisberger@gmail.com>
2026-08-28 03:29:05 +0000

    an170: overnight desk refresh, news above events

--- app.js
+++ app.js
@@ -27,8 +27,8 @@
     requestAnimationFrame(function () { window.anTranslatePage(); });
   }
 
-  var DATA_URL = "live-data.json?v=an167";
-  var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an170";
+  var DATA_URL = "live-data.json?v=an170";
+  var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an130";
   var state = {
     data: null,
     newsletter: [],
