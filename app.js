68890f0a6dff552760358a8bf77e0239158549be
AsherWeisberger <asherweisberger@gmail.com>
2026-08-29 02:43:42 +0000

    an192: live-data with Hy4 tweet photo media_url, no rebuild

--- app.js
+++ app.js
@@ -27,7 +27,7 @@
     requestAnimationFrame(function () { window.anTranslatePage(); });
   }
 
-  var DATA_URL = "live-data.json?v=an191";
+  var DATA_URL = "live-data.json?v=an192";
   var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an130";
   var state = {
     data: null,
