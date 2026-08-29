9193e9b1899d565c384c5ace22d445ee8a8e9cda
AsherWeisberger <asherweisberger@gmail.com>
2026-08-29 05:08:03 +0000

    an198: 43-story deep harvest, every card has photo, World has 8

--- app.js
+++ app.js
@@ -27,7 +27,7 @@
     requestAnimationFrame(function () { window.anTranslatePage(); });
   }
 
-  var DATA_URL = "live-data.json?v=an197";
+  var DATA_URL = "live-data.json?v=an198";
   var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an130";
   var state = {
     data: null,
