4c34eb356290444fbced7f58eb848503b4a92f13
AsherWeisberger <asherweisberger@gmail.com>
2026-08-29 20:52:28 +0000

    an214: 4pm 87-story desk, Hy4 lead, Astra out

--- app.js
+++ app.js
@@ -35,7 +35,7 @@
     requestAnimationFrame(function () { window.anTranslatePage(); });
   }
 
-  var DATA_URL = "live-data.json?v=an213";
+  var DATA_URL = "live-data.json?v=an214";
   var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an130";
   var state = {
     data: null,
