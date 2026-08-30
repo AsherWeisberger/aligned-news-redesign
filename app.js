18fb0cc4310515ddc8e58cff565d3554069f2147
AsherWeisberger <asherweisberger@gmail.com>
2026-08-30 09:22:55 +0000

    an219: 4am 98-story desk, H3 Max Live lead, Hy4 pull

--- app.js
+++ app.js
@@ -35,7 +35,7 @@
     requestAnimationFrame(function () { window.anTranslatePage(); });
   }
 
-  var DATA_URL = "live-data.json?v=an218";
+  var DATA_URL = "live-data.json?v=an219";
   var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an130";
   var state = {
     data: null,
