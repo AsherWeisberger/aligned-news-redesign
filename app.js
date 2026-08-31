4af8bdf4977ee7144d680ac6a68b35735b08c563
AsherWeisberger <asherweisberger@gmail.com>
2026-08-31 12:21:20 +0000

    an231: Monday Unaligned, H3 Max went live

--- app.js
+++ app.js
@@ -36,7 +36,7 @@
   }
 
   var DATA_URL = "live-data.json?v=an230";
-  var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an130";
+  var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an231";
   var state = {
     data: null,
     newsletter: [],
