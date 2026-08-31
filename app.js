a695a4969e738e4d6238664e84d8637a777892aa
AsherWeisberger <asherweisberger@gmail.com>
2026-08-31 05:10:34 +0000

    an227 center phone wordmark on screen midline

--- app.js
+++ app.js
@@ -35,7 +35,7 @@
     requestAnimationFrame(function () { window.anTranslatePage(); });
   }
 
-  var DATA_URL = "live-data.json?v=an226";
+  var DATA_URL = "live-data.json?v=an227";
   var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an130";
   var state = {
     data: null,
