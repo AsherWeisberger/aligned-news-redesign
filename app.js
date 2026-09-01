20d6b50b6cd473e6b1344a08b050d58736eaca68
AsherWeisberger <asherweisberger@gmail.com>
2026-09-01 00:34:51 +0000

    an233 8pm 2026-08-31 desk, EU DSA ChatGPT lead, 139 stories

--- app.js
+++ app.js
@@ -35,7 +35,7 @@
     requestAnimationFrame(function () { window.anTranslatePage(); });
   }
 
-  var DATA_URL = "live-data.json?v=an232";
+  var DATA_URL = "live-data.json?v=an233";
   var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an130";
   var state = {
     data: null,
