f3f140fedb76502f2cfcf0212b3424a9134ed34c
AsherWeisberger <asherweisberger@gmail.com>
2026-09-01 08:34:50 +0000

    an236 4am 2026-09-01 desk, EU DSA ChatGPT lead, Nepal past 1,000

--- app.js
+++ app.js
@@ -35,7 +35,7 @@
     requestAnimationFrame(function () { window.anTranslatePage(); });
   }
 
-  var DATA_URL = "live-data.json?v=an235";
+  var DATA_URL = "live-data.json?v=an236";
   var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an130";
   var state = {
     data: null,
