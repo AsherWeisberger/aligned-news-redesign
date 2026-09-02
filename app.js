dc8382f9fd73a60b767932520f8a963d7e8ea1fe
AsherWeisberger <asherweisberger@gmail.com>
2026-09-02 05:10:53 +0000

    an248 midnight 2026-09-02 desk, Claude Fable 5.1 / Mythos 5.1 lead, 164 stories

--- app.js
+++ app.js
@@ -35,7 +35,7 @@
     requestAnimationFrame(function () { window.anTranslatePage(); });
   }
 
-  var DATA_URL = "live-data.json?v=an245";
+  var DATA_URL = "live-data.json?v=an248";
   var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an130";
   var state = {
     data: null,
