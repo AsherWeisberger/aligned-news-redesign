214f144721b8f76c79b585b1dc6bf5b59125f4b3
AsherWeisberger <asherweisberger@gmail.com>
2026-09-02 01:59:03 +0000

    an246 GEV center zoom, keyless search, Pages data layers

--- app.js
+++ app.js
@@ -4588,7 +4588,7 @@
     }
   }
 
-  var GODS_EYE_SRC = "gods-eye/index.html?v=an242";
+  var GODS_EYE_SRC = "gods-eye/index.html?v=an246";
   var godModeBootPromise = null;
 
   function ensureGodModeWidget() {
