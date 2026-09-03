2452db1a548a66f3caefe75ba436fb75bf6f6522
AsherWeisberger <asherweisberger@gmail.com>
2026-09-03 05:18:02 +0000

    an257 midnight 2026-09-03 desk, Muse Spark 1.3 lead, 187 stories

--- app.js
+++ app.js
@@ -35,7 +35,7 @@
     requestAnimationFrame(function () { window.anTranslatePage(); });
   }
 
-  var DATA_URL = "live-data.json?v=an256";
+  var DATA_URL = "live-data.json?v=an257";
   var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an130";
   var state = {
     data: null,
