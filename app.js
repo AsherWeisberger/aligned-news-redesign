c9d394d3d0473528191666d30860200bb2e47b8b
Asher Weisberger <asherweisberger@users.noreply.github.com>
2026-09-03 12:34:36 +0000

    Unaligned 2026-09-03: Spark 1.3 got cheap (an260)

--- app.js
+++ app.js
@@ -35,8 +35,8 @@
     requestAnimationFrame(function () { window.anTranslatePage(); });
   }
 
-  var DATA_URL = "live-data.json?v=an259";
-  var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an130";
+  var DATA_URL = "live-data.json?v=an260";
+  var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an260";
   var state = {
     data: null,
     newsletter: [],
