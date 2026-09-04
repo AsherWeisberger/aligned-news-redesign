87d49797113a8995cea2b19f73c5948f0bd0c3ee
AsherWeisberger <asherweisberger@gmail.com>
2026-09-04 12:43:24 +0000

    Unaligned 2026-09-04: Astra runs the computer (an265)

--- app.js
+++ app.js
@@ -35,8 +35,8 @@
     requestAnimationFrame(function () { window.anTranslatePage(); });
   }
 
-  var DATA_URL = "live-data.json?v=an264";
-  var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an130";
+  var DATA_URL = "live-data.json?v=an265";
+  var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an265";
   var state = {
     data: null,
     newsletter: [],
