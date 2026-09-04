b7832631478bab08f8b54b8bec1035aa2b57bae5
AsherWeisberger <asherweisberger@gmail.com>
2026-09-04 05:02:09 +0000

    an263 midnight 2026-09-04 desk, GPT 6 Astra lead, 208 stories

--- app.js
+++ app.js
@@ -35,7 +35,7 @@
     requestAnimationFrame(function () { window.anTranslatePage(); });
   }
 
-  var DATA_URL = "live-data.json?v=an262";
+  var DATA_URL = "live-data.json?v=an263";
   var NEWSLETTER_DATA_URL = "newsletter-data.json?v=an130";
   var state = {
     data: null,
